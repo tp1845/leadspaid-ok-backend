827b6f231a4d4bc15eb6bc64780154b3683f6023
netprince1210-dev <ameliagenius3@gmail.com>
2022-11-15 13:15:27 +0530

    Add Front Form to submit lead

--- core/routes/web.php
+++ core/routes/web.php
@@ -544,3 +544,6 @@ Route::get('placeholder-image/{size}', 'SiteController@placeholderImage')->name(
 
 Route::get('/{slug}', 'SiteController@pages')->name('pages');
 Route::get('/', 'SiteController@index')->name('home');
+
+Route::get('/campaign_form/{campaign_id}/{publisher_id}','CampaignFormController@campaign_form_view')->name('front_campaign_form.view');
+Route::post('/campaign_form','CampaignFormController@campaign_form_save')->name('front_campaign_form.save');
