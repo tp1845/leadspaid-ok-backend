2b0f754a79b3301bcfd13c68b7607ec00c1b8bbe
netprince1210-dev <ameliagenius3@gmail.com>
2022-11-26 10:58:02 +0530

    payment-commit

--- core/resources/views/templates/basic/advertiser/payments.blade.php
+++ core/resources/views/templates/basic/advertiser/payments.blade.php
@@ -214,7 +214,7 @@
                         </li>
                     </ul>
                 </div>
-                <p class="py-2">* Please note that 3% will be reflected in your Invoice.</p>
+                <p class="py-2">*Please note that the Current Card Service Charge is 3%</p>
             </div> 
         </div>
      </div>   
@@ -280,30 +280,7 @@
 
 </div>
 
-<div class="container-fluid position-relative px-0 d-none">
 
-    <form method="POST" action="{{ route('advertiser.payments.createsession') }}">
-        @csrf
-
-    </form>
-    <div>
-        <button class="btn btn--primary setup-payment">@lang('Set up Payment Method')</button>
-    </div>
-
-</div>
-
-
-<div class="container-fluid position-relative px-0 d-none">
-
-    <form method="POST" action="{{ route('ipn.current_advertiser_charge') }}">
-        @csrf
-        <div>
-            <button type="submit" class="btn btn--primary">@lang('Make a payment')</button>
-        </div>
-    </form>
-
-
-</div>
 
 <div class="row">
     <div class="col-lg-12 d d-flex justify-content-between align-items-end">
@@ -582,6 +559,83 @@
         </div>
     </div>
 </div>
+
+
+
+<!------------------->
+<div class="container-fluid position-relative px-0">
+
+    <form method="POST" action="{{ route('advertiser.payments.update') }}" enctype="multipart/form-data">
+        @csrf
+        <input type="hidden" value="{{ Auth::guard('advertiser')->user()->id }}" name="advertiser">
+
+        <div class="form-inline">
+            <div class="has_append input-group" style="width: 215px;">
+                <span class="form-text">Total Campaign Budget</span>
+            </div>
+            <div class="m-1">
+                <input type="number" value="{{ Auth::guard('advertiser')->user()->total_budget }}" name="total_budget" class="bg-white form-control text--black" placeholder="Sum of Budget of All ads" value="">
+            </div>
+        </div>
+        <div class="form-inline">
+            <div class="input-group has_append" style="width: 215px;">
+                <span class="form-text">Amount used before this day</span>
+            </div>
+            <div class="m-1">
+                <input type="text" value="{{ Auth::guard('advertiser')->user()->amount_used }}" name="amount_used" class=" bg-white form-control text--black" placeholder="Amount used yesterday" value="">
+
+            </div>
+        </div>
+        <div>
+            <button type="submit" class="btn btn--primary">@lang('Save changes')</button>
+        </div>
+    </form>
+
+    <div class="bg-white p-3 rounded-lg" style="width: 150px;position: absolute;right: 2rem;top: 0rem;">
+        <div class="text-center">
+            <span class="form-text text--black">Wallet Deposit</span>
+        </div>
+        <div class="m-1">
+            <span class="text--gray" value="">$</span>
+            <span class="text--gray" value="">{{ Auth::guard('advertiser')->user()->wallet_deposit }}</span>
+        </div>
+
+    </div>
+
+</div>
+<div class="container-fluid position-relative px-0 ">
+
+    <form method="POST" action="{{ route('advertiser.payments.createsession') }}">
+        @csrf
+
+    </form>
+    <div>
+        <button class="btn btn--primary setup-payment">@lang('Set up Payment Method')</button>
+    </div>
+
+</div>
+
+
+<div class="container-fluid position-relative px-0 ">
+
+    <form method="POST" action="{{ route('ipn.current_advertiser_charge') }}">
+        @csrf
+        <div>
+            <button type="submit" class="btn btn--primary">@lang('Make a payment')</button>
+        </div>
+    </form>
+
+
+</div>
+
+
+
+
+<!----------------------->
+
+
+
+
 
 
 
