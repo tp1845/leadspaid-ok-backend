c89096268f44e7819b06cb352404a39ad002fc73
netprince1210-dev <ameliagenius3@gmail.com>
2022-11-28 16:40:40 +0530

    hide email

--- core/resources/views/templates/basic/layouts/frontendLeadPaid.blade.php
+++ core/resources/views/templates/basic/layouts/frontendLeadPaid.blade.php
@@ -156,15 +156,9 @@
 
           <!-- footer section end -->
         </div> <!-- page-wrapper end -->
-
-
-
         <!-- bootstrap js -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
         @stack('script-lib')
         @stack('script')
-
-
         </body>
     </html>
