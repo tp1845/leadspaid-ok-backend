d2cc8a6a48983692e5629d5d0a61a64c835d5417
netprince1210-dev <ameliagenius3@gmail.com>
2022-12-02 11:24:49 +0530

    Update home-leadpaid2.blade.php

--- core/resources/views/templates/basic/home-leadpaid2.blade.php
+++ core/resources/views/templates/basic/home-leadpaid2.blade.php
@@ -1,13 +1,14 @@
 @extends($activeTemplate.'layouts.frontendLeadPaid')
 
 @section('content')
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap" rel="stylesheet">
      <section class="MainBanner-Home">
         <div class="container">
             <div class="row align-item-center justify-content-center align-items-center">
                 
                 <div class="col-lg-12">
                     <h1 class="h1">Generate Leads Directly</h1>
-                    <p>An Alternative Lead Source distinct from Search, display & Native Ads</p>
+                    <p>From High Ranking/ Authority Websites & Apps</p>
                     <a href="#" class="btn btn-secondary btn-lg my-2">Join As Advertiser</a>
                 </div>
             </div>
@@ -39,9 +40,9 @@
 
           <div class="col-lg-8">
            <div class="fw-normal-side">
-                <h2 class="fw-normal mb-5">An Alternative Lead Sources <br>
+                <h2 class="fw-normal mb-5">An Alternative Lead Source <br>
                 <span>distinct from Search, display & Native Ads</span></h2>
-            <p class="text2">LeadsPaid.com <a href="">generate leads</a> compared to <br>buying traffic from Search Ads or other forms of ads</p>
+            <p class="text2">LeadsPaid.com <a href="">generate leads</a> compared to <br>buying traffic</p>
            </div>
             
           </div><!-- /.col-lg-8 -->
@@ -60,18 +61,16 @@
 body {
     background: #e0e0e0 !important;
 }
+
 .fw-normal span {
-    font-size: 25px;
+    font-size: 34px;
     color: #1361b2;
+    font-weight: 500;
 }
 .fw-normal{
     color: #1361b2;
 }
 
-.text2{
-    text-align:left;
-        font-size: 20px;
-}
 .fw-normal-side {
     display: flex;
     flex-wrap: wrap;
@@ -94,6 +93,28 @@ body {
     font-weight: 300;
 }
 .marketing  h2.fw-normal.mb-5 {
-    font-size: 42px;
+    font-size: 47px;
 }
+.text2 a:hover {
+    color: #6c6c6c;
+}
+.text2 a {
+    color: #6c6c6c;
+}
+.text2 {
+    text-align: left;
+    font-size: 26px;
+    color: #6c6c6c;
+}
+
+
+@media only screen and (min-width: 768px) {
+.MainBanner-Home .h1 {
+    font-size: 81px !important;
+}
+.MainBanner-Home p {
+    font-size: 36px !important;
+    font-weight: 300;
+}
+    }
 </style>
