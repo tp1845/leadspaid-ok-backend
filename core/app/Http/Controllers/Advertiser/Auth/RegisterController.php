0aa1b9b301d4162a140eaa7153027521b6457581
netprince1210-dev <ameliagenius3@gmail.com>
2022-12-03 16:06:10 +0530

    Update RegisterController.php

--- core/app/Http/Controllers/Advertiser/Auth/RegisterController.php
+++ core/app/Http/Controllers/Advertiser/Auth/RegisterController.php
@@ -107,7 +107,7 @@ class RegisterController extends Controller
 		
         $data=$this->decode_arr($request->code_verifiyed);
 	
-       return $user = $this->guard()->user($data['userid']);
+       return $user = 'test:'.$this->guard()->user($data['userid']);
 		  
         if ($this->checkValidCode_adv($user, $user->ver_code, 2)) {
             $target_time = $user->ver_code_send_at->addMinutes(2)->timestamp;
