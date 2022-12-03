9c2a1d92ef3add0a1e37a24c1695aafdd89c5562
netprince1210-dev <ameliagenius3@gmail.com>
2022-12-03 15:55:57 +0530

    email changes

--- core/app/Http/Controllers/Advertiser/Auth/RegisterController.php
+++ core/app/Http/Controllers/Advertiser/Auth/RegisterController.php
@@ -104,10 +104,11 @@ class RegisterController extends Controller
     }
 
     public function varify_adv(Request $request){
-		//return $request;
-		//die();
+		
         $data=$this->decode_arr($request->code_verifiyed);
+		
         $user = $this->guard()->user($data['userid']);
+		return $user;
         if ($this->checkValidCode_adv($user, $user->ver_code, 2)) {
             $target_time = $user->ver_code_send_at->addMinutes(2)->timestamp;
             $delay = $target_time - time();
