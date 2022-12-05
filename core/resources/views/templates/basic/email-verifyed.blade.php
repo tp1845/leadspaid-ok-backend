4f12f9e1cb9ca9767aab13782e43927d16fc3f32
netprince1210-dev <ameliagenius3@gmail.com>
2022-12-05 10:16:33 +0530

    500 error issue on register adviser

---
+++ core/resources/views/templates/basic/email-verifyed.blade.php
@@ -0,0 +1,30 @@
+@extends($activeTemplate.'layouts.frontendLeadPaid')
+
+@section('content')
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://leadspaid.com/assets/templates/basic/css/all.min.css">
+<section class="Rg_advts">
+    <div class="container">
+        
+        <div class="row text-center">
+            <div class="col-lg-12 pt-4">
+               <p class="Rg_advts_ttls mb-5">thanks for verify your account. you account will activate soon</p>
+            </div>
+        </div>
+           
+    </div>
+ </section>
+    
+   
+@endsection
+
+@push('script-lib')
+<script src="{{asset('assets/templates/basic')}}/js/vendor/particles.js"></script>
+<script src="{{asset('assets/templates/basic')}}/js/vendor/app.js"></script>    
+
+@endpush
+
+
+
+
+
