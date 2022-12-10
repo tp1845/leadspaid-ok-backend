c00eb83dddd4d0b49cc3a494fbed51ec35951d5e
netprince1210-dev <ameliagenius3@gmail.com>
2022-12-10 19:48:46 +0530

    force to https url for production app

--- core/app/Providers/AppServiceProvider.php
+++ core/app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\GeneralSetting;
 use App\Language;
 use App\Page;
 use App\Extension;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +28,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         $activeTemplate = activeTemplate();
 
         $viewShare['general'] = GeneralSetting::first();
@@ -35,23 +40,23 @@ class AppServiceProvider extends ServiceProvider
         $viewShare['language'] = Language::all();
         $viewShare['pages'] = Page::where('tempname',$activeTemplate)->where('slug','!=','home')->get();
         view()->share($viewShare);
-        
+
 
         view()->composer('admin.partials.sidenav', function ($view) {
             $view->with([
-             
+
 
                 'pending_ticket_count'         => \App\SupportTicket::whereIN('status', [0,2])->count(),
                 'pending_deposits_count'    => \App\Deposit::pending()->count(),
                 'pending_withdraw_count'    => \App\Withdrawal::pending()->count(),
                 'pending_domain'               => \App\DomainVerifcation::pending()->count(),
-               
+
                 'banned_advertiser_count'      => \App\Advertiser::banned()->count(),
                 'banned_publisher_count'       => \App\Publisher::banned()->count(),
 
                 'email_unverified_advertiser'  => \App\Advertiser::emailUnverified()->count(),
                 'sms_unverified_advertiser'    => \App\Advertiser::smsUnverified()->count(),
-               
+
                 'email_unverified_publisher' => \App\Publisher::emailUnverified()->count(),
                 'sms_unverified_publisher'   => \App\Publisher::smsUnverified()->count(),
             ]);
@@ -65,7 +70,7 @@ class AppServiceProvider extends ServiceProvider
         });
 
         view()->composer([$activeTemplate.'sections.overview',$activeTemplate.'blogDetails',$activeTemplate.'partials.footer'], function ($view) {
-           
+
             $view->with([
                 'total_users' => \App\Advertiser::count(),
                 'total_publisher' => \App\Publisher::count(),
