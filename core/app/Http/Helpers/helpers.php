84b424899b6d03ff119ad571679eff45829d9fb2
netprince1210-dev <ameliagenius3@gmail.com>
2022-12-26 17:56:21 +0530

    Update Verify email template

--- core/app/Http/Helpers/helpers.php
+++ core/app/Http/Helpers/helpers.php
@@ -569,33 +569,13 @@ function send_email($user, $type = null, $shortCodes = [])
 function send_email_adv($user, $type = null, $link)
 {
     $general = GeneralSetting::first();
-    $email_template = \App\EmailTemplate::where('act', $type)->where('email_status', 1)->first();
-    if ($general->en != 1 || !$email_template) {
-        return;
-    }
-
-        $message ='
-        <html>
-        <head>
-          <title>email varification</title>
-        </head>
-        <body>
-          <p>Please verify your account on this link  <a href='.$link.'>Click here</a></p>
-        </body>
-        </html>
-        ';
-
-    $config = $general->mail_config;
-
-    if ($config->name == 'php') {
-        send_php_mail($user->email, $user->username, $general->email_from, $email_template->subj, $message);
-    } else if ($config->name == 'smtp') {
-        send_smtp_mail($config, $user->email, $user->username, $general->email_from, $general->sitetitle, $email_template->subj, $message);
-    } else if ($config->name == 'sendgrid') {
-        send_sendGrid_mail($config, $user->email, $user->username, $general->email_from, $general->sitetitle, $email_template->subj, $message);
-    } else if ($config->name == 'mailjet') {
-        send_mailjet_mail($config, $user->email, $user->username, $general->email_from, $general->sitetitle, $email_template->subj, $message);
-    }
+    $email_template = \App\EmailTemplate::where('act',$type)->where('email_status', 1)->first();
+    if ($general->en != 1 || !$email_template) { return; }
+    $sendto_email=$user->email;
+    $receiver_name = $user->username;
+    $subject= $email_template->subj;
+    $message = '<p>Please verify your account on this link  <a href='.$link.'>Click here</a></p>';
+    send_general_email($sendto_email, $subject, $message, $receiver_name);
 }
 
 function send_email_adv_activated($user, $type = null, $name)
@@ -630,7 +610,6 @@ function send_email_adv_activated($user, $type = null, $name)
         send_mailjet_mail($config, $user->email, $user->username, $general->email_from, $general->sitetitle, $email_sub, $message);
     }
 }
-
 
 function send_email_adv_admin($user, $type = null, $username)
 {
