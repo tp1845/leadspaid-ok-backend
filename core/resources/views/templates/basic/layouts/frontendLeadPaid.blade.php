3fcf441da8fbfa02952d267bee80de1559fd9d5d
netprince1210-dev <ameliagenius3@gmail.com>
2022-12-28 15:59:48 +0530

    Update layout css

--- core/resources/views/templates/basic/layouts/frontendLeadPaid.blade.php
+++ core/resources/views/templates/basic/layouts/frontendLeadPaid.blade.php
@@ -347,18 +347,9 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         @stack('script-lib')
         @stack('script')
-
-
-
-    @push('style')
-    <style>
-        @media screen and (max-width: 992px){ #MainNav .logo img{ max-width: 240px!important;}  }
-        .navbar-toggler-icon {
-            width: 1.3em;
-            height: 1.3em;
-        }
-    </style>
-
-    @endpush
+        <style>
+            @media screen and (max-width: 992px){ #MainNav .logo img{ max-width: 240px!important;}  }
+            .navbar-toggler-icon {  width: 1.3em;  height: 1.3em;  }
+        </style>
         </body>
     </html>
