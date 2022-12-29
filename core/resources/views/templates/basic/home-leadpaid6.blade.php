93a8751a3056641658e9050fe65991411b60be9a
netprince1210-dev <ameliagenius3@gmail.com>
2022-12-29 13:22:34 +0530

    Update home page text

--- core/resources/views/templates/basic/home-leadpaid6.blade.php
+++ core/resources/views/templates/basic/home-leadpaid6.blade.php
@@ -20,7 +20,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                   <h3> Great Leads · Great Sales!!!</h3>
+                   <h3> GREAT LEADS · GREAT SALES!!!</h3>
                 </div>
             </div>
         </div>
