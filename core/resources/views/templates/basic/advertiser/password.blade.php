b9f9e05804618f3f4f1cd74a16c5bc31e3bba6b0
netprince1210-dev <ameliagenius3@gmail.com>
2023-01-13 10:18:23 +0530

    commit changes...

--- core/resources/views/templates/basic/advertiser/password.blade.php
+++ core/resources/views/templates/basic/advertiser/password.blade.php
@@ -1,48 +1,77 @@
-@extends($activeTemplate.'layouts.advertiser.frontend')
+@extends('admin.layouts.app')
 
 @section('panel')
-    <div class="row justify-content-center mb-none-30">
+    <div class="row mb-none-30">
+        <div class="col-lg-3 col-md-3 mb-30">
 
-        <div class="col-lg-6 mb-30">
+            <div class="card b-radius--5 overflow-hidden">
+                <div class="card-body p-0">
+                    <div class="d-flex p-3 bg--primary">
+                        <div class="avatar avatar--lg">
+                            <img src="{{ getImage(imagePath()['profile']['admin']['path'].'/'. $admin->image,imagePath()['profile']['admin']['size'])}}" alt="@lang('Image')">
+                        </div>
+                        <div class="pl-3">
+                            <h4 class="text--white">{{__($admin->name)}}</h4>
+                        </div>
+                    </div>
+                    <ul class="list-group">
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            @lang('Name')
+                            <span class="font-weight-bold">{{ __($admin->name) }}</span>
+                        </li>
+
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            @lang('Username')
+                            <span  class="font-weight-bold">{{ __($admin->username) }}</span>
+                        </li>
+
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            @lang('Email')
+                            <span  class="font-weight-bold">{{ $admin->email }}</span>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-9 col-md-9 mb-30">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title mb-50 border-bottom pb-2">@lang('Change Password')</h5>
 
-                    <form id="change_password" action="{{ route('advertiser.password.update') }}" method="POST" enctype="multipart/form-data">
+                    <form action="{{ route('admin.password.update') }}" method="POST" enctype="multipart/form-data">
                         @csrf
 
-                        <div class="form-group">
-                            <label class="form-control-label">@lang('Old Password')</label>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="las la-key"></i></span>
-                                </div>
-                                <input class="form-control" type="password" name="old_password" required>
+                        <div class="form-group row">
+                            <label class="col-lg-3 col-form-label form-control-label">@lang('Password')</label>
+                            <div class="col-lg-9">
+
+                                <input class="form-control" type="password" placeholder="@lang('Password')" name="old_password">
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <label class="form-control-label">@lang('New password')</label>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="las la-key"></i></span>
-                                </div>
-                                <input class="form-control" type="password" name="password" required>
+                        <div class="form-group row">
+                            <label class="col-lg-3 col-form-label form-control-label">@lang('New Password')</label>
+                            <div class="col-lg-9">
+                                <input class="form-control" type="password" placeholder="@lang('New Password')" name="password">
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <label class="form-control-label">@lang('Confirm password')</label>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="las la-key"></i></span>
-                                </div>
-                                <input class="form-control" type="password" name="password_confirmation" required>
+                        <div class="form-group row">
+                            <label class="col-lg-3 col-form-label form-control-label">@lang('Confirm Password')</label>
+                            <div class="col-lg-9">
+                                <input class="form-control" type="password" placeholder="@lang('Confirm Password')" name="password_confirmation">
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <button type="submit" class="btn btn--primary btn-block btn-lg Rg_advts_my_btn rounded-0">@lang('Save Changes')</button>
+
+                        <div class="form-group row">
+
+                            <label class="col-lg-3 col-form-label form-control-label"></label>
+                            <div class="col-lg-9">
+                            <button type="submit" class="btn btn--primary btn-block btn-lg">@lang('Save Changes')</button>
+                            </div>
                         </div>
                     </form>
                 </div>
@@ -52,27 +81,5 @@
 @endsection
 
 @push('breadcrumb-plugins')
-    <a href="{{route('advertiser.profile')}}" class="profile_pass_setting rounded-0 btn btn-sm btn--primary box--shadow1 text--small" ><i class="fa fa-user"></i>@lang('Profile Setting')</a>
-@endpush
-
-@push('style')
-<style type="text/css">
- #change_password .form-group label {
-        font-size: 16px;
-        color: #1a273a;
-    }
-	
-	.profile_pass_setting {
-    font-size: 18px !important;
-    padding: .375rem .75rem;
-}
-
-    .Rg_advts_my_btn {
-    font-size: 18px !important;
-    padding: .375rem .75rem;
-    background-color: #4500dd!important;
-    font-weight: 500;
-}
-	
-</style>
+    <a href="{{route('admin.profile')}}" class="btn btn-sm btn--primary box--shadow1 text--small" ><i class="fa fa-user"></i>@lang('Profile Setting')</a>
 @endpush
