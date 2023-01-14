16ea5ad56ec42cba1f964f441513bb53556117f2
netprince1210-dev <ameliagenius3@gmail.com>
2023-01-14 14:00:54 +0600

    button

--- core/resources/views/templates/basic/advertiser/payments.blade.php
+++ core/resources/views/templates/basic/advertiser/payments.blade.php
@@ -415,10 +415,19 @@
                         <div class="border-bottom py-2 mb-2 d-flex justify-content-between">
                             <h4 class="">Payment Information</h4>
                             <div class="adsrock-icon setup-payment">
-                                <svg height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg">
+                                <?php 
+                                if($card_info->last4){?>
+                                    <svg height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M6 34.5v7.5h7.5l22.13-22.13-7.5-7.5-22.13 22.13zm35.41-20.41c.78-.78.78-2.05 0-2.83l-4.67-4.67c-.78-.78-2.05-.78-2.83 0l-3.66 3.66 7.5 7.5 3.66-3.66z" />
                                     <path d="M0 0h48v48h-48z" fill="none" />
                                 </svg>
+                                <?php }else{ ?>
+                                    <a class="btn btn--primary text-white create-campaign-btn">Add Payment Method (Credit/ Debit card)</a>
+                                <?php } ?>
+
+                           
+                            
+                          
                             </div>
                         </div>
                         <div class="adsrock-list pt-3">
