05195ed92243ec6d515bb574c00e2fcb08d886d9
netprince1210-dev <ameliagenius3@gmail.com>
2023-01-17 00:08:26 +0530

    Update HomePage bg

--- core/resources/views/templates/basic/home-leadpaid-live.blade.php
+++ core/resources/views/templates/basic/home-leadpaid-live.blade.php
@@ -430,9 +430,9 @@
 
         #MainHeroHeader{
             background-color: #1a273a;
-            background-image: url("{{url('/')}}/assets/images/homepage/home-hero-v9.3.jpg");
+            background-image: url("{{url('/')}}/assets/images/homepage/home-hero-v9.3-v1.jpg");
             @if(Request::get('bg') == 1 )
-            background-image: url("{{url('/')}}/assets/images/homepage/home-hero-v9.3-v1.jpg")!important;
+            background-image: url("{{url('/')}}/assets/images/homepage/home-hero-v9.3.jpg")!important;
             @endif
             background-position: center;
             background-size: cover;
