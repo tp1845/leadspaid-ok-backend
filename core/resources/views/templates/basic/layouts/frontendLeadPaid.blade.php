67b2443bd5f2ea41c662e443c02a55a1057c74d3
netprince1210-dev <ameliagenius3@gmail.com>
2023-01-19 20:56:44 +0530

    remove fornt ; from bottom

--- core/resources/views/templates/basic/layouts/frontendLeadPaid.blade.php
+++ core/resources/views/templates/basic/layouts/frontendLeadPaid.blade.php
@@ -141,7 +141,7 @@
           </footer>
           <!-- footer section end -->
         </div> <!-- page-wrapper end -->
-        @include('admin.partials.notify');
+        @include('admin.partials.notify')
         <!-- bootstrap js -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         @stack('script-lib')
