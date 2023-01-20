18f9957500b2920c9a973b54dc04bcd8d71c8822
netprince1210-dev <ameliagenius3@gmail.com>
2023-01-20 11:14:06 +0530

    commit changes..

--- core/resources/views/templates/basic/advertiser/partials/topnav.blade.php
+++ core/resources/views/templates/basic/advertiser/partials/topnav.blade.php
@@ -44,7 +44,11 @@
 .nav-payment ul{
   margin: 15px 0;
 }
-
+.navbar-user__thumb{
+      width: 30px !important;
+    height: 30px !important;
+    margin-right: 5px;
+}
 
 </style>
 <nav class="navbar-wrapper justify-content-between active">
