8bda28e2edfebfdb5e0612fbac379ab05bc67493
netprince1210-dev <ameliagenius3@gmail.com>
2023-01-20 17:16:05 +0530

    Update website url validation

--- core/resources/views/templates/basic/advertiser/register-advertiser.blade.php
+++ core/resources/views/templates/basic/advertiser/register-advertiser.blade.php
@@ -177,9 +177,16 @@ $("#full_name").capitalize();
     jQuery.validator.addMethod("valid_email", function(value, element) {
     return this.optional(element) || /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i.test(value);
     }, "Please enter a valid email address");
+
     jQuery.validator.addMethod("website", function(value, element) {
-    return this.optional(element) || /^([^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/i.test(value);
+        rawInput =value;
+        cleanInput = rawInput.replace('www.', '');
+        cleanInput = cleanInput.replace('http://', '');
+        cleanInput = cleanInput.replace('https://', '');
+        console.log('url:' + cleanInput);
+    return this.optional(element) || /^([^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/i.test(cleanInput);
     }, "Please enter a valid Website");
+
     jQuery.validator.addMethod("isdphone", function(value, element) {
         var isd = $('.country_code').val();
 
