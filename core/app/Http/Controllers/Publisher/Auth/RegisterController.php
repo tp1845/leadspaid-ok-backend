4aad68aae767a06f7175ed5c3a33724789277ce5
netprince1210-dev <ameliagenius3@gmail.com>
2023-01-21 23:12:55 +0530

    Update varify_publisher

--- core/app/Http/Controllers/Publisher/Auth/RegisterController.php
+++ core/app/Http/Controllers/Publisher/Auth/RegisterController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
 use App\Publisher;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Validator;
 
 
