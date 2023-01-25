3540f47b49690325e86f90e06fd24c8041c02bbb
netprince1210-dev <ameliagenius3@gmail.com>
2023-01-25 19:19:12 +0530

    commit changes..

--- core/resources/views/admin/advertiser/all.blade.php
+++ core/resources/views/admin/advertiser/all.blade.php
@@ -520,7 +520,7 @@ table thead tr th:before {
     bottom: 14px !important;
 }
 
-#apporved_list table.dataTable tbody tr td:nth-child(8) {
+ table.dataTable tbody tr td:nth-child(8) {
     height: 100px;
     white-space: pre-wrap;
     overflow-y: auto;
