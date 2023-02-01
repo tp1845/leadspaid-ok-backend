fb16005585969d21ad7876990550d12b4227b226
netprince1210-dev <ameliagenius3@gmail.com>
2023-02-01 19:44:10 +0530

    Update advertiser admin

--- core/resources/views/templates/basic/layouts/advertiser/master.blade.php
+++ core/resources/views/templates/basic/layouts/advertiser/master.blade.php
@@ -5,6 +5,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   @include('partials.seo')
   <title>{{ $general->sitename($page_title ?? '') }}</title>
   <!-- jQuery library -->
